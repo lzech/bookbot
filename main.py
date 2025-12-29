e9841db25629ab4ff995af062c1a801401b97c07
lzech <lasse.zech@gmail.com>
2025-12-29 13:15:32 +0100

    remove reliance on hardcoded filepath

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from stats import get_number_words
 from stats import count_characters
 from stats import sort_dict
+import sys
 
 def get_book_text(path):
     with open(path, 'r') as f:
@@ -17,8 +18,16 @@ def print_report(words_num, dic, path):
         if i[0].isalpha():
             report += str(i[0]) + ": " + str(i[1]) + "\n"
     return report
+
+
 def main():
-    path = "books/frankenstein.txt"
+    path = ""
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        path = sys.argv[1]
+
     number_words = get_number_words(get_book_text(path))
     sorted_dict = sort_dict(count_characters(get_book_text(path)))
     print(print_report(number_words, sorted_dict, path))
